262d5edd4a2db048d634f5cfa98f1ecd7a944f4c
jfeng21 <junru.feng@asu.edu>
2016-03-31 12:26:23 -0400

    Create Pairs.java

---
+++ Pairs.java
@@ -0,0 +1,12 @@
+/*
+Given N integers, count the total pairs of integers that have a difference of K
+Input Format
+The 1st line contains two integers N and K
+The 2nd line contains N integers that form the set. All the numbers are distince
+Output Format
+An integer that gives the number of pairs of numbers that have a difference of K
+Constraints:
+N<=10^5
+0<K<10^9
+Each integer will be greater than 0 and least K smaller than 2^31-1
+*/
