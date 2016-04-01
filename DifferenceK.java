c0ef6e9e4bdcd80d788a0fef927df39a7e9e7042
jfeng21 <junru.feng@asu.edu>
2016-04-01 17:34:36 -0400

    Update DifferenceK.java

--- DifferenceK.java
+++ DifferenceK.java
@@ -20,47 +20,50 @@ public class DifferenceK {
         System.out.println(res);
     }
     
-    static int KDifference(int[] a, int k){
-        
-        for(int i = 0; i< a.length; i++){
-       if(a[i]<0 || a.length<5 || a.length>100000){
-       System.out.println("Please input positive number 5<=n<=100000 ");
-       return 0;
-       }
-       if(k<0||k==0||k>1000000000){
-        System.out.println("Please input positive number k: 0<k<=10^9");
-       return 0;
-       } 
-         if( a[i]>2000000000){
-        System.out.println("Please input positive number: 0<n<=2*10^9");
-       return 0;
-       }       
-      
+     static int KDifference(int[] a, int k) {
+        //check n length
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 0 || a.length < 5 || a.length > 100000) {
+                System.out.println("Please input positive number 5<=n<=100000 ");
+                return 0;
+            }
+            //check k
+            if (k < 0 || k == 0 || k > 1000000000) {
+                System.out.println("Please input positive number k: 0<k<=10^9");
+                return 0;
+            }
+            //check number
+            if (a[i] > 2000000000) {
+                System.out.println("Please input positive number: 0<n<=2*10^9");
+                return 0;
+            }
+
         }
-        
-       int pairCount = 0;
-       int[] result;
-       Map<Integer, Integer> map = new HashMap<>();
-       for(int i = 0; i<a.length; i++){
-           int x = a[i];
-           //check duplicate
-           if(map.containsKey(x)){
-           System.out.println("input must be distinct");
-           return 0;
-           }
-       if(map.containsKey(x+k)){
-       continue;
-       }   
-         if(map.containsKey(x-k)){
-         //   result = new int[]{map.get(a[i]),i};
-         //   System.out.println(result);
-          pairCount++;
-       }
-           map.put(x, i);
-       }
-       return pairCount;
-        
+
+        int pairCount = 0;
+        int[] result;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            int x = a[i];
+            //check duplicate
+            if (map.containsKey(x)) {
+                System.out.println("input must be distinct");
+                return 0;
+            }
+            if (map.containsKey(x + k)) {
+                continue;
+            }
+            if (map.containsKey(x - k)) {
+                //   result = new int[]{map.get(a[i]),i};
+                //   System.out.println(result);
+                pairCount++;
+            }
+            map.put(x, i);
+        }
+        return pairCount;
+
     }
+
     
    
         
